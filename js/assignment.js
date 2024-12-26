24efb3b35ef397a79e3c3ac7343cecffdf2ac330
Paige Saint Germain <paigejerin@gmail.com>
2024-12-26 17:34:42 -0500

    completed Javascript assignment 02

--- js/assignment.js
+++ js/assignment.js
@@ -9,22 +9,37 @@ const submissionBtn = document.getElementById("submission-btn")
 
 // Declare your variables here.
 
+const resolution = "Live life the way YOU want to live.";
+
+const currentYear = "2025";
+
+let willMeetResolution = "Ask me in a couple months.";
+
 function updateYear() {
     // write the logic
+    yearElement.innerText = currentYear;
 
 }
 
 function updateResolution() {
     // Update this function 
+    resolutionElement.innerText = resolution;
 
+}
+
+function updateWillMeetResolution() {
+    willMeetResolutionElement.innerText = willMeetResolution;
 }
 
 function render() {
     // Finish writing this function
+    updateYear();
+    updateResolution();
+    updateWillMeetResolution();
 
 }
 
 submissionBtn.addEventListener("click", function () {
     // Finish writing this function
-
+    render();
 })
--- js/index.js
+++ js/index.js
@@ -2,3 +2,23 @@
 // that are not declared
 "use strict"
 
+/*
+
+const myName = "Paige";
+console.log(myName)             // Output: "Paige"
+console.log(typeof myName)      // Output: "string"
+
+const one = 1;
+console.log(one)                // Output: 1
+console.log(typeof one);        // Output: "number"
+
+let isMentor = true;            
+console.log(isMentor)           // Output: true
+console.log(typeof isMentor)    // Output: "boolean"
+
+isMentor = false;
+
+console.log(isMentor)           // Output: false
+console.log(typeof isMentor)    // Output: "boolean"
+
+*/
